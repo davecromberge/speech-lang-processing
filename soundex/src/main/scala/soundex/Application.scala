d2f96c91c60017430271a69d8cdeaf69f105f204
davecromberge <davecromberge@gmail.com>
2013-05-15 12:35:11 +0200

    Update Application.scala

--- soundex/src/main/scala/soundex/Application.scala
+++ soundex/src/main/scala/soundex/Application.scala
@@ -1,11 +1,20 @@
 package soundex
 
-object Application extends App {
-  override def main(args: Array[String]): Unit = {
+object Application {
+
+  def main(args: Array[String]) {
     if (args.isEmpty) {
-      println("Please provide a sequence of words as arguments.")
-      System.exit(1)
-    }
+      println("Please supply two words as arguments.")
+      System.exit(0)
+    } 
+    val codes = args.map(x => Soundex.encode(x))
+    args.zip(codes).map { case (word, code) => println(s"The soundex code for $word is $code") }
+    
+    if (codes.distinct.length == 1)
+      println("All words are similar.")
+    else
+      println("Words are not similar.")
+    
     System.exit(0)
   }
 }
