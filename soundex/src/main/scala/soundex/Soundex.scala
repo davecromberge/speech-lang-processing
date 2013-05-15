f60eeee7d5da24604867164144042a1097f2b121
davecromberge <davecromberge@gmail.com>
2013-05-15 12:35:55 +0200

    Update Soundex.scala

--- soundex/src/main/scala/soundex/Soundex.scala
+++ soundex/src/main/scala/soundex/Soundex.scala
@@ -3,20 +3,21 @@ package soundex
 object Soundex {
   val cutOff = 3
   val exclusions = Set('a', 'e', 'i', 'o', 'u', 'y', 'h', 'w')
-
+    
   val codes = Map(
     '1' -> "bfpv", '2' -> "cgjkqsxz", '3' -> "dt",
     '4' -> "l", '5' -> "mn", '6' -> "r")
 
   val charCode: Map[Char, Char] =
     for ((digit, chars) <- codes; char <- chars) yield (char -> digit)
+    
 
   def encode(word: String): String = {
     word.toLowerCase.head +:  word.toLowerCase.drop(1)
-							      .filterNot(exclusions)
-							      .map(charCode)
-							      .distinct
-							      .padTo(cutOff, '0')
-							      .take(cutOff)
+				              .filterNot(exclusions)
+					      .map(charCode)
+					      .foldRight[List[Char]](Nil)((x, acc) => if (!acc.isEmpty && x == acc.head) acc else (x +: acc)).mkString
+					      .padTo(cutOff, '0')
+					      .take(cutOff)
   }
 }
